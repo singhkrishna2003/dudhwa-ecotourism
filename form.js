2da3fe8c683332023ea08611f6540b16e11c3bc6
Krishna Singh <krishna.singh@klusterfirst.com>
2026-04-22 12:37:14 +0530

    Updated pages

--- form.js
+++ form.js
@@ -5,10 +5,10 @@ function sendToWhatsApp() {
     var phone = document.getElementById("phone").value.trim();
     var age = document.getElementById("age").value;
     var adults = document.getElementById("adults").value;
-    var children = document.getElementById("children").value;
+   // var children = document.getElementById("children").value;
     var nationality = document.getElementById("nationality").value.trim();
-    var date = document.getElementById("date").value;
-    var message = document.getElementById("message").value.trim();
+   // var date = document.getElementById("date").value;
+    //var message = document.getElementById("message").value.trim();
 
     if (!name || !phone || !date) {
         alert("Please fill all required fields");
@@ -22,14 +22,14 @@ function sendToWhatsApp() {
         "\uD83D\uDC64 Name: " + name + "\n" +
         "\uD83D\uDCDE Contact: " + phone + "\n" +
         "\uD83C\uDF82 Age: " + (age || "N/A") + "\n" +
-        "\uD83D\uDC6A Adults: " + (adults || "N/A") + "\n" +
-        "\uD83D\uDC76 Children: " + (children || "N/A") + "\n" +
+        "\uD83D\uDC6A Number of Persons: " + (adults || "N/A") + "\n" +
+        //"\uD83D\uDC76 Children: " + (children || "N/A") + "\n" +
         "\uD83C\uDF0D Nationality: " + (nationality || "N/A") + "\n" +
-        "\uD83D\uDCC5 Date: " + date + "\n\n" +
-        "\uD83D\uDCDD Message:\n" +
-        (message || "No additional message");
+       // "\uD83D\uDCC5 Date: " + date + "\n\n" +
+       // "\uD83D\uDCDD Message:\n" +
+     (message || "No additional message");
 
-    var whatsappNumber = "919260952442"; // ✅ Your number
+    var whatsappNumber = "919559829836"; // ✅ Your number
 
     var url =
         "https://wa.me/" +
